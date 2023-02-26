f301b965d7462e2f939597f5c5cd406c705f50b3
Jaeiya <exanix@gmail.com>
2023-02-25 17:13:20 -0800

    fix: remove dev-dependent code

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-export function throwThis(msg: string) {
-    throw Error(msg);
-}
-
-export function isDev() {
-    return process.env.NODE_ENV == 'development';
-}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import { ConsoleLogger } from './lib/logger.js';
-import { isDev, throwThis } from './lib/utils.js';
 
 // Add leading line for aesthetic reasons
 console.log('');
@@ -13,14 +12,6 @@ console.log('');
 // ConsoleLogger.info('TypeScript source map support installed');
 
 const [, , arg] = process.argv;
-
-if (arg && arg == 'throw') {
-  if (isDev()) {
-    throwThis('This should produce a TypeScript file stack trace');
-  } else {
-    throwThis('This should produce a JavaScript file stack trace');
-  }
-}
 
 const _con = ConsoleLogger;
 _con.print('y', 'test', 'Hello World!!');
