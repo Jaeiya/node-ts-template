2a02e445b090e3e00fdfc4a20aa105db892a6553
Jaeiya <exanix@gmail.com>
2023-02-25 20:00:22 -0800

    upd(main): add boilerplate code to main

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+import { createSpinner } from './lib/cli-spinner.js';
 import { ConsoleLogger } from './lib/logger.js';
+import { testGlobalLog } from './lib/utils.js';
 
 // Add leading line for aesthetic reasons
 console.log('');
@@ -11,7 +13,20 @@ console.log('');
 // smp.default.install();
 // ConsoleLogger.info('TypeScript source map support installed');
 
-const [, , arg] = process.argv;
 global._con = ConsoleLogger;
 
 _con.print('y', 'test', 'Hello World!!');
+_con.addCustomColor('p', 'FF83DD');
+_con.addCustomColor('rty', '2FD8A2');
+_con.addCustomColor('l', 'A2FF00');
+_con.addCustomColor('o', 'FFA800');
+_con.info('info message with ;p;custom pink ;x;text');
+_con.error('error message with ;by;bright yellow ;x;text');
+// testGlobalLog();
+const spinner = createSpinner(
+    _con.toString('by', 'spinner', ';l;@spin ;o;Some Orange Text ;l;@spin')
+);
+
+spinner.start();
+
+setTimeout(spinner.stop, 4000);
