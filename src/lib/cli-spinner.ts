947076816921c344d87dd69a1414683505840109
Jaeiya <exanix@gmail.com>
2023-02-25 16:34:07 -0800

    feat: add CLI spinner support

---
+++ src/lib/cli-spinner.ts
@@ -0,0 +1,47 @@
+import { stdout } from 'process';
+
+const _dotFrames = ['⠋', '⠙', '⠚', '⠞', '⠖', '⠦', '⠴', '⠲', '⠳', '⠓'];
+
+const _hideCursorChar = '\u001B[?25l';
+const _showCursorChar = '\u001B[?25h';
+
+export function createSpinner(text: string) {
+  let interval: NodeJS.Timer;
+
+  function start() {
+    if (!text.includes('@spin')) {
+      throw Error('missing "@spin" placeholder');
+    }
+    let framePos = 0;
+    writeSpinnerFrame(framePos++, text);
+    interval = setInterval(() => {
+      framePos = writeSpinnerFrame(framePos++, text);
+    }, 90);
+  }
+
+  return {
+    start,
+    stop: () => {
+      clearInterval(interval);
+      clearStdout();
+      stdout.write(_showCursorChar);
+    },
+  };
+}
+
+/**
+ * Writes a single frame of the dot array to console
+ * and returns the next frame position.
+ */
+function writeSpinnerFrame(framePos: number, msg: string) {
+  clearStdout();
+  if (framePos == _dotFrames.length) framePos = 0;
+  const str = msg.replaceAll('@spin', _dotFrames[framePos]);
+  stdout.write(`${str}${_hideCursorChar}`);
+  return ++framePos;
+}
+
+function clearStdout() {
+  stdout.clearLine(0);
+  stdout.cursorTo(0);
+}
